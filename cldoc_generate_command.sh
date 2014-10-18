185c7792e923b0e5b35b5fd960bc60f8b6d033b4
Danny McClanahan <danieldmcclanahan@gmail.com>
2014-10-17 23:47:52 -0500

    fixed cldoc generate

--- cldoc_generate_command.sh
+++ cldoc_generate_command.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-cldoc generate -std=c11 -I/usr/local/include/glib-2.0 -I/usr/local/lib/glib-2.0/include -Wall -Wextra -Werror -DSINGLE_START_CODON -DECOLI -DDEBUG -- --output ../cldoc_gen --language c --basedir . src/*
+cldoc generate -std=c11 "$(pkg-config --cflags glib-2.0)" \
+    -Wall -Wextra -Werror -DSINGLE_START_CODON -DECOLI -DDEBUG -- \
+    --output ../cldoc_gen --language c --basedir . src/*
