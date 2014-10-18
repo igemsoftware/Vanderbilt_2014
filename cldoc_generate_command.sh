26bd891b64bb93c40b228be9e165b046f237f24c
Danny McClanahan <danieldmcclanahan@gmail.com>
2014-10-17 19:32:28 -0500

    feverishly adding API documentation

---
+++ cldoc_generate_command.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cldoc generate -std=c11 -I/usr/local/include/glib-2.0 -I/usr/local/lib/glib-2.0/include -Wall -Wextra -Werror -DSINGLE_START_CODON -DECOLI -DDEBUG -- --output docs/cldoc_gen --language c --basedir . src/*
--- src/vcsfmt.h
+++ src/vcsfmt.h
@@ -9,49 +9,78 @@
 #include "block_processing.h" // for block processing methods
 
 #define OUTPUT_SUFFIX ".vcsfmt"
-#define META_SUFFIX	".vcsfmt.meta.tmp"
+#define META_SUFFIX ".vcsfmt.meta.tmp"
 
-// TODO: javadoc
-// if filetype supported, produces vcsfmt file of same name
+/* cldoc:begin-category(vcsfmt) */
+
+/* Produces formatted file with extension ".vcsfmt".
+ * @filename the file to produce a vcsfmt file from.
+ *
+ * vcsfmt file produced in same directory as given file.
+ */
 void vcsfmt(char * filename);
-void de_vcsfmt(char * filename); // produces original file
+/* Produces unformatted file from vcsfmt file.
+ * @filename the file to produce an unformatted version of.
+ *
+ * Unformatted file produced in same directory as vcsfmt file.
+ */
+void de_vcsfmt(char * filename);
 
-/**
- * Tranforms a block of fasta formatted ascii data into pure genetic data that can
- * be processed by the functions in block_processing.h.
+/* cldoc:end-category() */
+
+/* cldoc:begin-category(fasta_preformat) */
+
+/* Tranforms block of fasta data into pure genetic data.
+ * @input the input string of fasta data
+ * @output a raw string of memory to write unformatted genetic data into
+ * @metadata a string giving additional information about the fasta data
+ * @in_comment whether or not we are currently in a fasta comment.
+ * @lines_processed the number of lines previously processed
  *
- * Basically removes new lines and meta-data. The meta-data for each block is separated out
- * into a separate structure that is annotated in such a way that the original fasta file can
- * be recovered by calling de_fasta_preformat. The meta-data returned from each call should be
- * written to a separate file, which can be appended to the end of the vcsfmt file.
+ * Tranforms a block of fasta formatted ascii data into pure genetic data that
+ * can be processed by the functions in block_processing.h.
  *
- * This function is capable of processing large files in chunks. Some parameters, like in_comment,
- * should be passed in to each sequential call so that the function remembers key information about
- * the last chunk it processed.
+ * Basically removes new lines and meta-data. The meta-data for each block is
+ * separated out into a separate structure that is annotated in such a way that
+ * the original fasta file can be recovered by calling de_fasta_preformat. The
+ * meta-data returned from each call should be written to a separate file, which
+ * can be appended to the end of the vcsfmt file.
  *
- * @precondition	the size of output in memory is >= the size of input in memory.
+ * This function is capable of processing large files in chunks. Some
+ * parameters, like in_comment, should be passed in to each sequential call so
+ * that the function remembers key information about the last chunk it
+ * processed.
  *
- * @param 	in_comment 	Whether or not we are currently in a fasta comment.
+ * Precondition: size of output in memory >= the size of input in memory.
+ *
+ * @return a raw block of genetic data
  */
 string_with_size * fasta_preformat(string_with_size * input,
-									string_with_size * output,
-									string_with_size * metadata,
-									bool * in_comment,
-									int * lines_processed);
+                                   string_with_size * output,
+                                   string_with_size * metadata,
+                                   bool * in_comment,
+                                   int * lines_processed);
 
-/**
- * Writes a header line to a VCSFMT file, with information needed to reconstruct the FASTA file exactly
- * from the VCSFMT file.
+/* Write header to vcsfmt file.
+ * @vcsfmt_file the formatted file to write to
+ * @fasta_file the input file to read header data from
+ * @metadata_byte_length the length of data in the header
+ *
+ * Writes a header line to a VCSFMT file, with information needed to reconstruct
+ * the FASTA file exactly from the VCSFMT file.
  */
 void fasta_write_header(FILE * vcsfmt_file,
-						FILE * fasta_file,
-						long metadata_byte_length);
+                        FILE * fasta_file,
+                        long metadata_byte_length);
 
-/**
- * A helper function for preformat that writes a line annotation to a string buffer.
+/* A helper function for preformat that writes a line annotation to a string.
+ * @output the string buffer to write to
+ * @line_number the current index of the line in file
  *
- * returns the number of characters written.
+ * @return the number of characters written.
  */
 int write_annotation(char * output, int line_number);
+
+/* cldoc:end-category() */
 
 #endif /*___VCS_FMT_H___*/
